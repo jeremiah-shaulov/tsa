5fadf0dd95899670fc3313cc07e741f80d8e256e
jeremiah-shaulov <jeremiah.shaulov@gmail.com>
2023-06-30 19:01:14 +0300

    To typescript@5.1.6

--- private/tsa_ns.ts
+++ private/tsa_ns.ts
@@ -1,4 +1,4 @@
-import typescript from 'npm:typescript@5.1.3';
+import typescript from 'npm:typescript@5.1.6';
 import {createDenoProgram} from './create_deno_program.ts';
 import {LoadOptions} from './load_options.ts';
 import {DocNode} from './types/mod.ts';
@@ -11,8 +11,10 @@ type Any = any;
 
 (typescript as Any).createDenoProgram = createDenoProgram;
 
-declare module 'npm:typescript@5.1.3'
-{	type DenoProgram = Program & {emitDoc(options?: EmitDocOptions): DocNode[]};
+declare module 'npm:typescript@5.1.6'
+{	interface DenoProgram extends Program
+	{	emitDoc(options?: EmitDocOptions): DocNode[];
+	}
 
 	function createDenoProgram(entryPoints: readonly string[], compilerOptions?: CompilerOptions, loadOptions?: LoadOptions): Promise<DenoProgram>;
 }
