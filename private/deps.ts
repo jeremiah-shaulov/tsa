ebcbf24b1ceddde5ff891fcf3562506086e9c2d1
jeremiah-shaulov <jeremiah.shaulov@gmail.com>
2024-11-09 17:37:18 +0200

    To crc32hash@v2.0.1

--- private/deps.ts
+++ private/deps.ts
@@ -5,7 +5,7 @@ export {cache, directory as cacheDirectory} from 'https://deno.land/x/cache@0.2.
 export {resolveImportMap, resolveModuleSpecifier, type ImportMap} from 'https://deno.land/x/importmap@0.2.1/mod.ts';
 export {Command} from 'https://deno.land/x/cmd@v1.2.0/mod.ts';
 export {indentAndWrap} from 'https://deno.land/x/indent_and_wrap@v0.0.17/mod.ts';
-export {crc32} from 'https://deno.land/x/crc32hash@v2.0.0/mod.ts';
+export {crc32} from 'https://deno.land/x/crc32hash@v2.0.1/mod.ts';
 export {jstok, TokenType as JstokTokenType} from 'https://deno.land/x/jstok@v2.0.0/mod.ts';
 export * as path from 'jsr:@std/path@1.0.8';
 export * as semver from 'jsr:@std/semver@1.0.3';
